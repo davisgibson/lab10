304f072eddbbb3ab610c6f7092cad11337214dbb
davisgibson <juicepluskid2@yahoo.com>
2014-10-21 13:49:47 -0500

    Green grass and a red house

--- 70-100pt.py
+++ 70-100pt.py
@@ -23,19 +23,22 @@ drawpad = Canvas(root, width=800,height=600, background='white')
 drawpad.grid(row=0, column=1)
 
 #outline of house
-rectangle1 = drawpad.create_rectangle(300,200,500,400)
+rectangle1 = drawpad.create_rectangle(300,200,500,400,fill='red')
 line1 = drawpad.create_line(300,200,400,100)
 line2 = drawpad.create_line(500,200,400,100)
 
 #door and windows
-door = drawpad.create_rectangle(380,330,420,400)
+door = drawpad.create_rectangle(380,330,420,400,fill='white')
 doorHandle = drawpad.create_oval(385,360,392,367)
-window1 = drawpad.create_rectangle(325,230,375,280)
-window2 = drawpad.create_rectangle(425,230,475,280)
+window1 = drawpad.create_rectangle(325,230,375,280,fill='white')
+window2 = drawpad.create_rectangle(425,230,475,280,fill='white')
 
 #chimney
-#chimney = drawpad.create_rectangle(330,50,360,170)
 chimney1 = drawpad.create_line(330,50,330,170)
 chimney2 = drawpad.create_line(360,50,360,140)
 chimneyTop = drawpad.create_line(330,50,360,50)
+
+#green grass
+ground = drawpad.create_rectangle(200,400,600,500,fill='green')
+
 root.mainloop()
